e4b7e2ab3ad4da8202ceef726defe60ae765c871
Lazar Nagulov <lazarnagulov1@gmail.com>
2026-02-02 11:03:55 +0100

    Add environment variables

--- server/main.py
+++ server/main.py
@@ -1,7 +1,16 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from flask import Flask
 
 
 app = Flask(__name__)
+load_dotenv(Path(__file__).resolve().parent / ".env")
+
+INFLUX_TOKEN  = os.getenv("INFLUX_TOKEN")
+INFLUX_ORG    = os.getenv("INFLUX_ORG")
+INFLUX_URL    = os.getenv("INFLUX_URL")
+INFLUX_BUCKET = os.getenv("INFLUX_BUCKET")
 
 
 if __name__ == "__main__":
