e04802c3e849bce3b0438a5d471338cb626b22e8
FilipT03 <filipsombor@gmail.com>
2026-02-03 22:19:20 +0100

    Remove print

--- server/mqtt/client.py
+++ server/mqtt/client.py
@@ -10,9 +10,6 @@ def on_connect(client, userdata, flags, reason_code, properties):
         print("MQTT connect failed:", reason_code)
 
 def on_message(client, userdata, msg):
-    print(f"MQTT message received on topic {msg.topic}")
-    print(msg.payload.decode())
-    print(msg)
     payload = json.loads(msg.payload.decode())
     save_to_db(payload)
 
