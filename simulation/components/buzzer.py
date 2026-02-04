b0130e61ad0657f612bbe3b60a3bf6f95339fcdf
Lazar Nagulov <lazarnagulov1@gmail.com>
2026-02-04 09:42:27 +0100

    hotfix: Actuator not publishing state

--- simulation/components/buzzer.py
+++ simulation/components/buzzer.py
@@ -4,6 +4,7 @@ from typing import List
 from actuators.actuator_registry import ActuatorRegistry
 from config import BuzzerConfig
 from actuators.actuator_state import ActuatorState
+from util.event_bus import EventBus
 from simulators.actuator import run_actuator_simulator
 from util.logger import get_logger
 
@@ -12,13 +13,13 @@ logger = get_logger()
 def buzzer_changed(name: str, is_on: ActuatorState) -> None:
     logger.info(f"{name} is now {'ON' if is_on else 'OFF'}")
 
-def run_buzzer(config: BuzzerConfig, registry: ActuatorRegistry,  threads: List[threading.Thread], stop_event: threading.Event) -> None:
+def run_buzzer(config: BuzzerConfig, registry: ActuatorRegistry, event_bus: EventBus, threads: List[threading.Thread], stop_event: threading.Event) -> None:
     actuator = registry.get(config.id)
     if config.simulated:
         logger.info(f"Starting {config.id} Simulator")
         buzzer_thread = threading.Thread(
             target=run_actuator_simulator,
-            args=(actuator, buzzer_changed, stop_event),
+            args=(actuator, config, event_bus, buzzer_changed, stop_event),
             daemon=True
         )
         buzzer_thread.start()
--- simulation/simulators/actuator.py
+++ simulation/simulators/actuator.py
@@ -3,10 +3,14 @@ from typing import Callable
 
 from actuators.actuator_registry import Actuator
 from actuators.actuator_state import ActuatorState
+from config import DeviceConfig
+from util.event_bus import EventBus, SensorEvent
 
 
 def run_actuator_simulator(
     actuator: Actuator, 
+    device_config: DeviceConfig,
+    event_bus: EventBus,
     callback: Callable[[str, ActuatorState], None], 
     stop_event: threading.Event
 ) -> None:
@@ -18,4 +22,5 @@ def run_actuator_simulator(
 
         if current != last_state:
             callback(actuator.name, current)
+            event_bus.publish(SensorEvent(device_config, { "toggle": current.value }))
             last_state = current
